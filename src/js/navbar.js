8075328158415ddb5ac018a4adaf586b97971fd8
Blake Kaufman <blakekaufman9@gmail.com>
2024-08-27 16:22:43 -0400

    fixed navbar img sourcing issue

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -2,18 +2,35 @@
 
 const hamburgerMenuBTN = document.querySelector(".mobileNavBTN");
 const mobileNavContent = document.querySelector(".mobileNavDropdown");
-const navigationIcon = document.querySelector(".navigationIcon");
+const navigationIcon = document.querySelectorAll(".navigationIcon");
+
+console.log(navigationIcon);
+navigationIcon.forEach((item) => {
+  console.log(item);
+});
 
 function handleMobileNavClick() {
   console.log(window.scrollY);
   if (mobileNavContent.style.display === "flex") {
     mobileNavContent.style.display = "none";
     document.body.style.overflow = "scroll";
-    navigationIcon.src = "./src/assets/images/hamburger.svg";
+    navigationIcon.forEach((item, index) => {
+      console.log(item);
+      item.classList.remove("shwon");
+      item.classList.remove("hidden");
+      if (index == 0) item.classList.add("shown");
+      else item.classList.add("hidden");
+    });
   } else {
     mobileNavContent.style.display = "flex";
     document.body.style.overflow = "hidden";
-    navigationIcon.src = "./src/assets/images/xIcon.svg";
+    navigationIcon.forEach((item, index) => {
+      console.log(item);
+      item.classList.remove("shwon");
+      item.classList.remove("hidden");
+      if (index == 1) item.classList.add("shown");
+      else item.classList.add("hidden");
+    });
   }
 }
 
