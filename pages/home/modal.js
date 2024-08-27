a509bb43d761a288f4b6d2f4e48c3708e52af008
Blake Kaufman <blakekaufman9@gmail.com>
2024-08-27 16:44:37 -0400

    fixed modal offset with scroll

--- pages/home/modal.js
+++ pages/home/modal.js
@@ -3,6 +3,8 @@ const modalContainer = document.querySelector(".modalContainer");
 const modal = document.querySelector(".modal");
 const appCards = document.querySelectorAll(".appCard");
 
+console.log((modalContainer.style.top = 50));
+
 function toggleModal(e) {
   e.preventDefault();
 
@@ -10,6 +12,10 @@ function toggleModal(e) {
   console.log("test");
   modalContainer.style.display =
     modalContainer.style.display === "flex" ? "none" : "flex";
+  document.body.style.overflow =
+    modalContainer.style.display === "flex" ? "hidden" : "scroll";
+
+  modalContainer.style.top = `${window.scrollY}px`;
 }
 console.log(appCards);
 
