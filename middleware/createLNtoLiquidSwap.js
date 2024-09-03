7b1a57033ac57f6ad4d761c9e985aac715797081
Blake Kaufman <blakekaufman9@gmail.com>
2024-09-03 17:15:36 -0400

    created ln to liquid swap creator

---
+++ middleware/createLNtoLiquidSwap.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+import { ECPairFactory } from "ecpair";
+import { Transaction, address, crypto, networks } from "liquidjs-lib";
+import { randomBytes } from "liquidjs-lib/src/psetv2/utils";
+import ecc from "@bitcoinerlab/secp256k1";
+
+export default async function createLNtoLiquidSwap(
+  invoiceAmount,
+  liquidAddress
+) {
+  console.log("RUNNING IN FUNC");
+  // Create a random preimage for the swap; has to have a length of 32 bytes
+  const preimage = randomBytes(32);
+  const keys = ECPairFactory(ecc).makeRandom();
+
+  const signature = keys.signSchnorr(
+    crypto.sha256(Buffer.from(process.env.TESTET_ADDRESS, "utf-8"))
+  );
+
+  console.log(process.env.TESTET_ADDRESS);
+  // Create a Submarine Swap
+  const createdResponse = await fetch(
+    `${process.env.BOLTZ_API_URL}swap/reverse`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        address: process.env.TESTET_ADDRESS,
+        addressSignature: signature.toString("hex"),
+        invoiceAmount: Number(invoiceAmount),
+        to: "L-BTC",
+        from: "BTC",
+        claimPublicKey: keys.publicKey.toString("hex"),
+        preimageHash: crypto.sha256(preimage).toString("hex"),
+        // referralId: "blitzWallet",
+        // description: description || "Send to Blitz Wallet",
+      }),
+    }
+  );
+
+  return { createdResponse: await createdResponse.json(), keys, preimage };
+}
