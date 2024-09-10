0007eab8aed93415e98f954be8d094725f00fc8f
Blake Kaufman <blakekaufman9@gmail.com>
2024-09-10 16:58:24 -0400

    created add data to database method

--- netlify/databaseActions.js
+++ netlify/databaseActions.js
@@ -1,6 +1,6 @@
 "use strict";
 import "dotenv/config";
-import { signIn } from "../db";
+import { addDataToCollection, signIn } from "../db";
 import { decrypt } from "../middleware/encription";
 import { decryptMessage } from "../middleware/newEncription";
 
@@ -13,27 +13,60 @@ export async function handler(event, context) {
       postData.content
     );
     const databaseMethod = decryptedContent.type.toLowerCase();
+    const userPubKey = postData.pubKey;
+    const collectionName = decryptedContent.collectionName;
 
-    if (databaseMethod === "adddata") {
-      //Add data to database and return if succesful or not
-    } else if (databaseMethod == "getdata") {
-      //get and return data to device
-    } else if (databaseMethod === "deletedata") {
-      //delete data from database and return if successful or not
-    } else if (databaseMethod === "validuniquename") {
-      //check if is valid and return value
-    } else if (databaseMethod === "singlecontact") {
-      //gets single contact and returns contact
-    } else if (databaseMethod === "validposname") {
-      //check if is valid and return value
-    } else if (databaseMethod === "searchusers") {
-      //returns a list of users based on search parameter
-    } else {
+    try {
+      if (databaseMethod === "adddata") {
+        const didAddData = await addDataToCollection(
+          decryptedContent.dataObject,
+          collectionName,
+          userPubKey
+        );
+        if (didAddData) {
+          return {
+            statusCode: 200,
+            body: JSON.stringify({
+              status: "SUCCESS",
+              reason: "",
+            }),
+          };
+        } else {
+          return {
+            statusCode: 400,
+            body: JSON.stringify({
+              status: "ERROR",
+              reason: "Not able to add data to database",
+            }),
+          };
+        }
+      } else if (databaseMethod == "getdata") {
+        //get and return data to device
+      } else if (databaseMethod === "deletedata") {
+        //delete data from database and return if successful or not
+      } else if (databaseMethod === "validuniquename") {
+        //check if is valid and return value
+      } else if (databaseMethod === "singlecontact") {
+        //gets single contact and returns contact
+      } else if (databaseMethod === "validposname") {
+        //check if is valid and return value
+      } else if (databaseMethod === "searchusers") {
+        //returns a list of users based on search parameter
+      } else {
+        return {
+          statusCode: 400,
+          body: JSON.stringify({
+            status: "ERROR",
+            reason: "Not a valid DB method",
+          }),
+        };
+      }
+    } catch (err) {
       return {
         statusCode: 400,
         body: JSON.stringify({
           status: "ERROR",
-          reason: "Not a valid DB method",
+          reason: "Calling database method failed",
         }),
       };
     }
