f396c32980595c9cc8860cfff806d034b4c6d212
Blake Kaufman <blakekaufman9@gmail.com>
2024-09-10 16:18:00 -0400

    adding methods to database

--- db/index.js
+++ db/index.js
@@ -1,36 +1,17 @@
-// Import the functions you need from the SDKs you need
-import {
-  initializeAuth,
-  signInAnonymously,
-  signInWithCustomToken,
-} from "firebase/auth";
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
 import * as admin from "firebase-admin";
-
+import { initializeAuth, signInWithCustomToken } from "firebase/auth";
+import { getApps, initializeApp } from "firebase/app";
 import {
   getFirestore,
   collection,
-  addDoc,
   doc,
-  setDoc,
-  Firestore,
-  getDocFromServer,
-  getDocs,
   getDoc,
-  deleteDoc,
+  setDoc,
   query,
   where,
+  getDocs,
 } from "firebase/firestore";
 
-// Optionally import the services that you want to use
-// import {...} from "firebase/auth";
-// import {...} from "firebase/database";
-// import {...} from "firebase/firestore";
-// import {...} from "firebase/functions";
-// import {...} from "firebase/storage";
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API,
   authDomain: "blitz-wallet-82b39.firebaseapp.com",
@@ -40,15 +21,13 @@ const firebaseConfig = {
   appId: "1:129198472150:web:86511e5250364ee1764277",
 };
 
-// Initialize the Firebase Admin SDK
-// const app = admin.initializeApp({
-//   projectId: "blitz-wallet-82b39",
-//   credential: admin.credential.applicationDefault(),
-//   databaseURL: "https://BlitzWalletUsers.firebaseio.com",
-// });
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig, "blitzWallet");
+} else {
+  app = getApps()[0]; // use the already initialized app
+}
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = initializeAuth(app);
 
@@ -57,22 +36,19 @@ export async function addDataToCollection(dataObject, collection, uuid) {
     const docRef = doc(db, `${collection}/${uuid}`);
 
     let docData = dataObject;
-    // console.log(docData, 'DOC DATA');
+
     docData["uuid"] = uuid;
+
     setDoc(docRef, docData, { merge: true });
 
-    console.log("Document written with ID: ", docRef.id);
-    return new Promise((resolve) => {
-      resolve(true);
-    });
+    console.log("Document written with ID: ", docData);
+
+    return true;
   } catch (e) {
     console.error("Error adding document: ", e);
-    return new Promise((resolve) => {
-      resolve(false);
-    });
+    return false;
   }
 }
-
 export async function getDataFromCollection(collectionName, uuid) {
   try {
     const docRef = doc(db, `${collectionName}`, `${uuid}`);
@@ -81,47 +57,57 @@ export async function getDataFromCollection(collectionName, uuid) {
     if (docSnap.exists()) {
       const data = docSnap.data();
 
-      return new Promise((resolve) => {
-        resolve(data);
-      });
-    } else throw new Error("error");
+      return data;
+    } else return false;
   } catch (err) {
-    return new Promise((resolve) => {
-      resolve(false);
-    });
     console.log(err);
+    return false;
   }
 }
-
 export async function deleteDataFromCollection(collectionName, uuid) {
   try {
-    const data = await getDataFromCollection(collectionName, uuid);
+    const docRef = doc(db, `${collectionName}/${uuid}`);
+    const respones = await deleteDoc(docRef);
 
-    let newObject = {};
-
-    newObject["amountSent"] = data["amountSent"];
-    newObject["uuid"] = data["uuid"];
-    newObject["k1"] = data["k1"];
-    newObject["totalAmountToSend"] = data["totalAmountToSend"];
-    newObject["pushToken"] = null;
-
-    await addDataToCollection(newObject, collectionName, uuid);
-
-    return new Promise((resolve) => {
-      resolve(true);
-    });
+    return true;
   } catch (err) {
-    return new Promise((resolve) => {
-      resolve(false);
-    });
     console.log(err);
+    return false;
   }
 }
-export async function getSignleContact(wantedName) {
-  const token = await signIn();
-  try {
-    if (!token) throw new Error("ERROR");
 
+export async function isValidUniqueName(collectionName, wantedName) {
+  try {
+    const userProfilesRef = collection(db, collectionName);
+    const q = query(
+      userProfilesRef,
+      where(
+        "contacts.myProfile.uniqueNameLower",
+        "==",
+        wantedName.toLowerCase()
+      )
+    );
+    const querySnapshot = await getDocs(q);
+    return querySnapshot.empty;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+}
+
+export async function queryContacts(collectionName) {
+  try {
+    const snapshot = await getDocs(collection(db, collectionName));
+
+    return snapshot["docs"];
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+}
+
+export async function getSignleContact(wantedName) {
+  try {
     const userProfilesRef = collection(db, "blitzWalletUsers");
 
     const q = query(
@@ -132,31 +118,80 @@ export async function getSignleContact(wantedName) {
     const querySnapshot = await getDocs(q);
     // Extract the data from each document
     const userData = querySnapshot.docs.map((doc) => doc.data());
-    return new Promise((resolve) => resolve(userData));
+    return userData;
   } catch (err) {
     console.log(err);
-    return new Promise((resolve) => resolve(false));
+    return false;
   }
 }
-async function signIn() {
+export async function canUsePOSName(
+  collectionName = "blitzWalletUsers",
+  wantedName
+) {
   try {
-    const customClaim = await createCustomToken();
+    const userProfilesRef = collection(db, collectionName);
+    const q = query(
+      userProfilesRef,
+      where("posSettings.storeNameLower", "==", wantedName.toLowerCase())
+    );
+    const querySnapshot = await getDocs(q);
+    return querySnapshot.empty;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+}
+
+// Function to search users by username
+export async function searchUsers(searchTerm) {
+  if (!searchTerm) return []; // Return an empty array if the search term is empty
+
+  try {
+    const usersRef = collection(db, "blitzWalletUsers");
+    const q = query(
+      usersRef,
+      where(
+        "contacts.myProfile.uniqueNameLower",
+        ">=",
+        searchTerm.toLowerCase()
+      ),
+      where(
+        "contacts.myProfile.uniqueNameLower",
+        "<=",
+        searchTerm.toLowerCase() + "\uf8ff"
+      )
+    );
+    const querySnapshot = await getDocs(q);
+
+    const users = querySnapshot.docs.map((doc) => {
+      return doc.data()?.contacts?.myProfile;
+    });
+    return users;
+  } catch (error) {
+    console.error("Error searching users: ", error);
+    return false; //needd to equal empty arrray
+  }
+}
+
+export async function signIn() {
+  try {
+    const customClaim = await createCustomFBToken();
     if (!customClaim) throw Error("NO CLAIM TOKEN CREATED");
-    return true;
+    return customClaim;
   } catch (error) {
     console.error("Error signing in anonymously", error);
     return false;
   }
 }
 
-async function createCustomToken() {
+async function createCustomFBToken() {
   try {
     const token = await admin
       .auth()
       .createCustomToken(process.env.FIREBASE_AUTH_CODE);
     await signInWithCustomToken(auth, token);
 
-    return true;
+    return token;
   } catch (error) {
     console.error("Error creating custom token:", error);
     return false;
