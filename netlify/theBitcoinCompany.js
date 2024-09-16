f9af57a9d83435aaf11bff9562f52143e7086686
Blake Kaufman <blakekaufman9@gmail.com>
2024-09-16 11:53:31 -0400

    created sign up path

--- netlify/theBitcoinCompany.js
+++ netlify/theBitcoinCompany.js
@@ -1,7 +1,8 @@
 "use strict";
 import "dotenv/config";
 import { JWTAuth } from "../middleware/JWTAuth";
-import { decryptMessage } from "../middleware/newEncription";
+import { decryptMessage, encryptMessage } from "../middleware/newEncription";
+import e from "express";
 
 const serverURL =
   process.env.BOLTZ_ENVIRONMENT === "liquid"
@@ -9,91 +10,110 @@ const serverURL =
     : "https://api.dev.thebitcoincompany.com";
 export async function handler(event, context) {
   if (event.httpMethod === "POST") {
-    const postData = event.body ? JSON.parse(event.body) : null; //sanitation
-    const token = event.headers.authorization;
-    const decryptedContent = JSON.parse(
-      await decryptMessage(
-        process.env.DB_PRIVKEY,
-        postData.pubKey,
-        postData.content
-      )
-    );
+    try {
+      const postData = event.body ? JSON.parse(event.body) : null; //sanitation
+      const token = event.headers.authorization;
+      //   const decryptedContent = JSON.parse(
+      //     await decryptMessage(
+      //       process.env.DB_PRIVKEY,
+      //       postData.pubKey,
+      //       postData.content
+      //     )
+      //   );
 
-    const isAuthenticated = JWTAuth(token);
+      const isAuthenticated = JWTAuth(token);
 
-    if (decryptedContent.type === "listGiftCards") {
-      try {
-        const response = await fetch(`${serverURL}/giftcards`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        const data = await response.json();
+      if (postData.type === "listGiftCards") {
+        try {
+          const response = await fetch(`${serverURL}/giftcards`, {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+          const data = await response.json();
 
-        return {
-          statusCode: 200,
-          body: JSON.stringify({
-            giftCards: data.result.svs,
-          }),
-        };
-      } catch (err) {
-        return {
-          statusCode: 400,
-          body: JSON.stringify({
-            error: "Error getting options",
-          }),
-        };
+          return {
+            statusCode: 200,
+            body: JSON.stringify({
+              giftCards: data.result.svs,
+            }),
+          };
+        } catch (err) {
+          return {
+            statusCode: 400,
+            body: JSON.stringify({
+              error: "Error getting options",
+            }),
+          };
+        }
+      } else if (postData.type === "signUp") {
+        try {
+          const response = await fetch(`${serverURL}/auth/sign-up`, {
+            method: "POST",
+            body: JSON.stringify({
+              email: postData.email,
+              password: postData.password,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+          const data = await response.json();
+          if (data.statusCode === 400)
+            return {
+              statusCode: 400,
+              body: JSON.stringify({
+                error: data.error,
+              }),
+            };
+          //   const encriptedContact = encryptMessage(
+          //     process.env.DB_PRIVKEY,
+          //     userPubKey,
+          //     JSON.stringify(data)
+          //   );
+          return {
+            statusCode: 200,
+            body: JSON.stringify({
+              response: data,
+            }),
+          };
+        } catch (err) {
+          console.log(err, "TESt");
+          return {
+            statusCode: 400,
+            body: JSON.stringify(err),
+          };
+        }
       }
-    } else if (decryptedContent.type === "signUp") {
-      try {
-        const response = await fetch(`${serverURL}/auth/sign-up`, {
-          method: "POST",
-          body: JSON.stringify({
-            email: decryptedContent.email,
-            password: decryptedContent.password,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        const data = await response.json();
+      // if (!token)
+      //   return {
+      //     statusCode: 400,
+      //     body: JSON.stringify({
+      //       error: "Error no authentication token provided",
+      //     }),
+      //   };
 
-        return {
-          statusCode: 200,
-          body: JSON.stringify({
-            response: data.result,
-          }),
-        };
-      } catch (err) {
-        return {
-          statusCode: 400,
-          body: JSON.stringify({
-            error: "Error getting options",
-          }),
-        };
-      }
+      // if (!isAuthenticated)
+      //   return {
+      //     statusCode: 400,
+      //     body: JSON.stringify({
+      //       error: "Incorrect authenticatoin token",
+      //     }),
+      //   };
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify("WORKIGN"),
+      };
+    } catch (err) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          error: "Error getting options",
+        }),
+      };
     }
-    // if (!token)
-    //   return {
-    //     statusCode: 400,
-    //     body: JSON.stringify({
-    //       error: "Error no authentication token provided",
-    //     }),
-    //   };
-
-    // if (!isAuthenticated)
-    //   return {
-    //     statusCode: 400,
-    //     body: JSON.stringify({
-    //       error: "Incorrect authenticatoin token",
-    //     }),
-    //   };
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify("WORKIGN"),
-    };
   } else {
     return {
       statusCode: 400,
