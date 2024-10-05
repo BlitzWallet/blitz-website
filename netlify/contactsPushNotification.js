1696443d0df2b9baa1535352cdaf0bc1e5190770
Blake Kaufman <blakekaufman9@gmail.com>
2024-10-05 12:48:18 -0400

    added JWT to request

--- netlify/contactsPushNotification.js
+++ netlify/contactsPushNotification.js
@@ -2,11 +2,36 @@
 import "dotenv/config";
 import { decrypt } from "../middleware/encription";
 import { sendContactNotification } from "../middleware/sendContactNotification";
+import { JWTAuth } from "../middleware/JWTAuth";
 
 export async function handler(event, context) {
   const data = event.body ? JSON.parse(event.body) : null; //sanitation
   if (event.httpMethod === "POST") {
     try {
+      if (!data)
+        return {
+          statusCode: 400,
+          body: JSON.stringify({
+            error: "Must have a body",
+          }),
+        };
+      if (!data.token)
+        return {
+          statusCode: 400,
+          body: JSON.stringify({
+            error: "",
+          }),
+        };
+
+      const isAuthenticated = JWTAuth(data.token);
+
+      if (!isAuthenticated)
+        return {
+          statusCode: 400,
+          body: JSON.stringify({
+            error: "Incorrect authenticatoin token",
+          }),
+        };
       const devicePushKey = data.devicePushKey;
       const deviceType = data.deviceType;
       const message = data.message;
