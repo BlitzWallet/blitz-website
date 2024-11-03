b104b5cc61352d9523a0ad3909bd345c4c579f3b
Blake Kaufman <blakekaufman9@gmail.com>
2024-11-03 13:31:16 -0500

    re-addeed notify function

--- netlify/notify.js
+++ netlify/notify.js
@@ -1,52 +1,73 @@
-// "use strict";
-// import "dotenv/config";
-// import { Expo } from "expo-server-sdk";
+"use strict";
+import "dotenv/config";
+import { Expo } from "expo-server-sdk";
+import { getSignleContact } from "../db";
+import { decrypt } from "../middleware/encription";
 
-// let expo = new Expo({ accessToken: process.env.PUSH_NOTIFICATION_SECRET });
+let expo = new Expo({ accessToken: process.env.PUSH_NOTIFICATION_SECRET });
 
-// export async function handler(event, context) {
-//   if (event.httpMethod === "POST") {
-//     const Parameters = event.queryStringParameters
-//       ? event.queryStringParameters
-//       : null;
-//     const Data = event.body ? JSON.parse(event.body) : null; //sanitation
+export async function handler(event, context) {
+  if (event.httpMethod === "POST") {
+    const Parameters = event.queryStringParameters
+      ? event.queryStringParameters
+      : null;
+    const Data = event.body ? JSON.parse(event.body) : null; //sanitation
 
-//     if (!Data || !Parameters)
-//       return {
-//         statusCode: 400,
-//         body: JSON.stringify({ error: "Error with request" }),
-//       };
-//     try {
-//       await expo.sendPushNotificationsAsync([
-//         {
-//           to: `${Parameters.token}`,
-//           sound: "default",
-//           // aps: {
-//           //   "content-available": 1,
-//           // },
-//           _contentAvailable: true,
-//           mutableContent: true,
-//           priority: "high",
-//           // title: "Blitz Wallet",
-//           // body: `Sent from Backend`,
-//           data: Data,
-//         },
-//       ]);
-//       return {
-//         statusCode: 200,
-//         body: JSON.stringify({ error: "Working" }),
-//       };
-//     } catch (err) {
-//       console.log(err);
-//       return {
-//         statusCode: 200,
-//         body: JSON.stringify({ error: "Error" }),
-//       };
-//     }
-//   } else {
-//     return {
-//       statusCode: 400,
-//       body: JSON.stringify({ error: "Must be a post request" }),
-//     };
-//   }
-// }
+    console.log(Parameters);
+
+    if (!Data || !Parameters)
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: "Error with request" }),
+      };
+    try {
+      const contact = await getSignleContact(
+        "blitzWalletUsers",
+        Parameters.token.toLowerCase()
+      );
+      if (!contact[0]?.pushNotifications?.key?.encriptedText) {
+        return {
+          statusCode: 400,
+          body: JSON.stringify({
+            status: "ERROR",
+            reason: "User does not have push notifications turned on",
+          }),
+        };
+      }
+      const devicePushKey = decrypt(
+        contact[0].pushNotifications.key.encriptedText
+      );
+      await expo.sendPushNotificationsAsync([
+        {
+          to: `${devicePushKey}`,
+          sound: "default",
+          // aps: {
+          //   "content-available": 1,
+          // },
+          _contentAvailable: true,
+          mutableContent: true,
+          priority: "high",
+          title: "Blitz Wallet",
+
+          body: "Handling payment in the background",
+          data: Data,
+        },
+      ]);
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ error: "Working" }),
+      };
+    } catch (err) {
+      console.log(err);
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ error: "Error" }),
+      };
+    }
+  } else {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Must be a post request" }),
+    };
+  }
+}
