18d8d86f05619011db1141d4b4acd6f2230e72b0
Blake Kaufman <blakekaufman9@gmail.com>
2024-11-10 12:32:18 -0500

    finished slildeshow section

--- pages/home/slideshow.js
+++ pages/home/slideshow.js
@@ -9,16 +9,25 @@ const contentOrder = [
     heading: "Explore the Blitz Store",
     subHeading:
       " Discover a seamless in-app store experience, designed to make spending your Bitcoin as easy as a few taps. Browse a curated selection of items and services, pay instantly, and enjoy the convenience of secure, wallet-integrated shopping.",
+    image: "./src/assets//images/blitzStorePage.png",
   },
   {
     heading: "Effortless Transactions with Contacts",
     subHeading:
       "Simplify your Bitcoin transfers with Blitz Wallet's Contacts feature. Save and manage your trusted contacts to send and receive payments with just a tap, making peer-to-peer transactions more convenient and secure than ever.",
+    image: "./src/assets//images/blitzContactPage.png",
   },
   {
     heading: "Transaction History",
     subHeading:
       "Keep an eye on every Bitcoin transaction with a detailed history at your fingertips. Access past payments, monitor spending, and stay in control with clear insights into your activity.",
+    image: "./src/assets//images/blitzTransactions.png",
+  },
+  {
+    heading: "Point-of-sale",
+    subHeading:
+      "The point-of-sale (POS) system is designed to enable businesses to easily accept Bitcoin payments. This integrated POS solution provides a user-friendly interface for merchants to generate payment requests and receive bitcoin to the creators wallet.",
+    image: "./src/assets//images/posPage.png",
   },
 ];
 
@@ -27,6 +36,10 @@ function handleSlideShowClick(e) {
   const targetElementClassList = Array.from(targetElement.classList);
   if (!targetElementClassList.includes("sliderBTNImages")) return;
   const container = targetElement.parentElement.parentElement;
+
+  const iphone = Array.from(container.parentElement.parentElement.children)[1]
+    .children[0];
+
   const containerChildren = Array.from(container.children);
   const header = containerChildren[0];
   const subHeader = containerChildren[1];
@@ -49,6 +62,7 @@ function handleSlideShowClick(e) {
     subHeader.innerHTML = contentOrder[currentIndex].subHeading;
     header.style.opacity = 1;
     subHeader.style.opacity = 1;
+    iphone.src = contentOrder[currentIndex].image;
     currentTimeout = null; // Clear the reference after timeout completes
   }, 400);
 }
