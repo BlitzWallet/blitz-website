b6a3b1a93e196f9721eb3b7cd8c518395b4182fc
Blake Kaufman <blakekaufman9@gmail.com>
2024-12-04 12:03:00 -0500

    remove decrypt path

--- netlify/decriptMessage.js
+++ netlify/decriptMessage.js
@@ -1,29 +1,36 @@
-"use strict";
-import "dotenv/config";
-import { decrypt } from "../middleware/encription";
+// "use strict";
+// import "dotenv/config";
+// import { decrypt } from "../middleware/encription";
 
-export async function handler(event, context) {
-  if (event.httpMethod === "POST") {
-    const Data = event.body ? JSON.parse(event.body) : null; //sanitation
-    if (!Data)
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ error: "Error with request" }),
-      };
+// export async function handler(event, context) {
+//   return {
+//     statusCode: 400,
+//     body: JSON.stringify({
+//       status: "ERROR",
+//       reason: "Must be a post request",
+//     }),
+//   }
+//   if (event.httpMethod === "POST") {
+//     const Data = event.body ? JSON.parse(event.body) : null; //sanitation
+//     if (!Data)
+//       return {
+//         statusCode: 400,
+//         body: JSON.stringify({ error: "Error with request" }),
+//       };
 
-    const decrypted = decrypt(Data.encriptedText);
+//     const decrypted = decrypt(Data.encriptedText);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ decryptedText: decrypted }),
-    };
-  } else {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({
-        status: "ERROR",
-        reason: "Must be a post request",
-      }),
-    };
-  }
-}
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({ decryptedText: decrypted }),
+//     };
+//   } else {
+//     return {
+//       statusCode: 400,
+//       body: JSON.stringify({
+//         status: "ERROR",
+//         reason: "Must be a post request",
+//       }),
+//     };
+//   }
+// }
