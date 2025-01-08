164c3406b3f233508fc5a4433da46ff165f62176
Blake Kaufman <blakekaufman9@gmail.com>
2025-01-08 16:40:28 -0500

    finshed blog post template

--- pages/blog/blogContentList.js
+++ pages/blog/blogContentList.js
@@ -5,11 +5,17 @@ export const postList = [
     type: "Release", // Can be "Release", "Tutorial", or "Podcast"
     title: "Introducing Blitz Wallet",
     description: "A bitcoin wallet making payments simple",
-    htmlPageLink: "./template",
+    htmlPageLink: "./introducing-blitz-wallet",
+    searchName: "introducing-blitz-wallet",
+    pageMetaDescription:
+      "Learn how Blitz Wallet simplifies self-custodial Bitcoin Lightning transactions. This guide dives into its innovative features, and seamless integration with the Bitcoin Lightning Network. Explore why Blitz Wallet is the ultimate self-custodial Bitcoin Lightning wallet!",
+    readingLengthEST: 6, //In minutes
     time: "1736275644454", // January 8, 2025
-    imageUrl: "../../src/assets/images/megaphone.svg",
-    thumbnailUrl: "../../public/wordmark.png",
-    isFeatured: true,
+    author: "Blitz Team",
+    authorImage: "../../../public/favicon.png", //Scoped to blog post page or public depending on image
+    imageUrl: "../../src/assets/images/megaphone.svg", //Scoped to blog index.html| Megaphone is for Release, Info is for Tutorial and Microphone is for podcast
+    thumbnailUrl: "../../public/wordmark.png", //Scoped to blog index.html
+    isFeatured: true, // usualy will be set to false
   },
 ];
 
@@ -18,6 +24,11 @@ export const getPostsByCategory = (category) => {
   if (!category || category.toLowerCase() === "all") return postList;
   return postList.filter((post) =>
     category.toLowerCase().includes(post.type.toLowerCase())
+  );
+};
+export const getPostByName = (postTitle) => {
+  return postList.filter(
+    (post) => postTitle.toLowerCase() === post.searchName.toLowerCase()
   );
 };
 
---
+++ pages/blog/pageSetup.js
@@ -0,0 +1,47 @@
+import { formatDate, getPostByName } from "./blogContentList.js";
+const blogTypeImage = document.getElementById("blogTypeImage");
+const blogTypeText = document.getElementById("blogTypeText");
+const blogTitle = document.getElementById("blogTitle");
+const blogSubHeader = document.getElementById("blogSubHeader");
+const authorName = document.getElementById("authorName");
+const postReadTime = document.getElementById("postReadTime");
+const postDate = document.getElementById("postDate");
+
+const authorProfilePicture = document.getElementById("authorProfilePicture");
+window.addEventListener("load", () => {
+  const url = window.location.href;
+  const urlParams = new URL(url.toString());
+  const postTitle = urlParams.pathname.split("/")?.[3];
+
+  console.log(url, urlParams, postTitle);
+  const [post] = getPostByName(postTitle);
+  document.title = `Blitz Wallet | ${post.title}`;
+  blogTypeImage.src = `../${post.imageUrl}`;
+  blogTypeImage.alt = `Image of a ${
+    post.type.toLowerCase() === "release"
+      ? "megaphone"
+      : post.type.toLowerCase() === "tutorial"
+      ? "question mark"
+      : "microphone"
+  } to show that post is of type ${post.type.toLowerCase()}`;
+  blogTypeText.textContent = ` ${post.type}`;
+  blogTitle.textContent = post.title;
+  blogSubHeader.textContent = post.description;
+  authorProfilePicture.src = post.authorImage;
+  authorName.textContent = post.author;
+  postReadTime.textContent = `${post.readingLengthEST} minutes`;
+  postDate.textContent = formatDate(post.time);
+  setMetaDescription(post.pageMetaDescription);
+});
+
+function setMetaDescription(newDescription) {
+  if (!document.querySelector('meta[name="description"]')) {
+    const metaDescription = document.createElement("meta");
+    metaDescription.name = "description";
+    metaDescription.content = newDescription;
+    document.head.appendChild(metaDescription);
+  } else
+    document
+      .querySelector('meta[name="description"]')
+      .setAttribute("content", newDescription);
+}
