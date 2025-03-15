a4adc825e55cfb293827677f0dfbb5935d1e0dc0
Blake Kaufman <blakekaufman9@gmail.com>
2025-03-15 12:50:03 -0400

    posting new update

--- pages/blog/blogContentList.js
+++ pages/blog/blogContentList.js
@@ -3,19 +3,55 @@
 export const postList = [
   {
     type: "Release", // Can be "Release", "Learn", or "Podcast"
-    title: "Introducing Blitz Wallet",
-    description: "A bitcoin wallet making payments simple",
-    htmlPageLink: "./introducing-blitz-wallet",
-    searchName: "introducing-blitz-wallet",
+    title: "The Power Up",
+    description: "Ecash overhall, POS Tip Tracking & More!",
+    htmlPageLink: "./power-up",
+    searchName: "power-up",
     pageMetaDescription:
       "Blitz Wallet is a fully open-source Bitcoin Lightning, Liquid, and eCash wallet that merges the fast capabilities of the Lightning Network with the security of self-custodial management at a price unmatched by competitors.",
-    readingLengthEST: 5, //In minutes
-    time: "1736994574819", // January 8, 2025
+    readingLengthEST: 4, //In minutes
+    time: "1742057210850",
     author: "Blitz Team",
     authorImage: "../../../public/favicon.png", //Scoped to blog post page or public depending on image
     imageUrl: "../../src/assets/images/megaphone.svg", //Scoped to blog index.html| Megaphone is for Release, Info is for Learn and Microphone is for podcast
-    thumbnailUrl: "./introducing-blitz-wallet/assets/thumbnailUrlWhite.png", //Scoped to blog index.html
-    isFeatured: true, // usualy will be set to false
+    thumbnailUrl: "./power-up/assets/posTransactions.webp", //Scoped to blog index.html
+    isVertical: true,
+    isFeatured: false, // usualy will be set to false
+  },
+  {
+    type: "Learn", // Can be "Release", "Learn", or "Podcast"
+    title: "What Is a Self-Custodial Wallet?",
+    description: "Your Guide to Financial Freedom with Blitz Wallet.",
+    htmlPageLink: "./self-custodial-wallet",
+    searchName: "self-custodial-wallet",
+    pageMetaDescription:
+      "Blitz Wallet is a fully open-source Bitcoin Lightning, Liquid, and eCash wallet that merges the fast capabilities of the Lightning Network with the security of self-custodial management at a price unmatched by competitors.",
+    readingLengthEST: 7, //In minutes
+    time: "1741611775672",
+    author: "Destiny Smart",
+    authorImage: "../../../public/authorImages/destiny_smart.webp", //Scoped to blog post page or public depending on image
+    imageUrl: "../../src/assets/images/info.svg", //Scoped to blog index.html| Megaphone is for Release, Info is for Learn and Microphone is for podcast
+    thumbnailUrl: "./self-custodial-wallet/assets/self_custody.png", //Scoped to blog index.html
+    isVertical: true,
+    isFeatured: false, // usualy will be set to false
+    customAuthor: true,
+  },
+  {
+    type: "Release", // Can be "Release", "Learn", or "Podcast"
+    title: "The Efficiency Update",
+    description: "Saying goodbye to long load times.",
+    htmlPageLink: "./efficiency-update",
+    searchName: "efficiency-update",
+    pageMetaDescription:
+      "Blitz Wallet is a fully open-source Bitcoin Lightning, Liquid, and eCash wallet that merges the fast capabilities of the Lightning Network with the security of self-custodial management at a price unmatched by competitors.",
+    readingLengthEST: 2, //In minutes
+    time: "1740237953923",
+    author: "Blitz Team",
+    authorImage: "../../../public/favicon.png", //Scoped to blog post page or public depending on image
+    imageUrl: "../../src/assets/images/megaphone.svg", //Scoped to blog index.html| Megaphone is for Release, Info is for Learn and Microphone is for podcast
+    thumbnailUrl: "./efficiency-update/assets/homepage.png", //Scoped to blog index.html
+    isVertical: true,
+    isFeatured: false, // usualy will be set to false
   },
   {
     type: "Release", // Can be "Release", "Learn", or "Podcast"
@@ -36,55 +72,19 @@ export const postList = [
   },
   {
     type: "Release", // Can be "Release", "Learn", or "Podcast"
-    title: "The Efficiency Update",
-    description: "Saying goodbye to long load times.",
-    htmlPageLink: "./efficiency-update",
-    searchName: "efficiency-update",
+    title: "Introducing Blitz Wallet",
+    description: "A bitcoin wallet making payments simple",
+    htmlPageLink: "./introducing-blitz-wallet",
+    searchName: "introducing-blitz-wallet",
     pageMetaDescription:
       "Blitz Wallet is a fully open-source Bitcoin Lightning, Liquid, and eCash wallet that merges the fast capabilities of the Lightning Network with the security of self-custodial management at a price unmatched by competitors.",
-    readingLengthEST: 2, //In minutes
-    time: "1740237953923",
+    readingLengthEST: 5, //In minutes
+    time: "1736994574819", // January 8, 2025
     author: "Blitz Team",
     authorImage: "../../../public/favicon.png", //Scoped to blog post page or public depending on image
     imageUrl: "../../src/assets/images/megaphone.svg", //Scoped to blog index.html| Megaphone is for Release, Info is for Learn and Microphone is for podcast
-    thumbnailUrl: "./efficiency-update/assets/homepage.png", //Scoped to blog index.html
-    isVertical: true,
-    isFeatured: false, // usualy will be set to false
-  },
-  // {
-  //   type: "Release", // Can be "Release", "Learn", or "Podcast"
-  //   title: "The Power Up",
-  //   description: "Ecash overhall, POS Tip Tracking & More!",
-  //   htmlPageLink: "./power-up",
-  //   searchName: "power-up",
-  //   pageMetaDescription:
-  //     "Blitz Wallet is a fully open-source Bitcoin Lightning, Liquid, and eCash wallet that merges the fast capabilities of the Lightning Network with the security of self-custodial management at a price unmatched by competitors.",
-  //   readingLengthEST: 4, //In minutes
-  //   time: "1741383361036",
-  //   author: "Blitz Team",
-  //   authorImage: "../../../public/favicon.png", //Scoped to blog post page or public depending on image
-  //   imageUrl: "../../src/assets/images/megaphone.svg", //Scoped to blog index.html| Megaphone is for Release, Info is for Learn and Microphone is for podcast
-  //   thumbnailUrl: "./power-up/assets/posTransactions.webp", //Scoped to blog index.html
-  //   isVertical: true,
-  //   isFeatured: false, // usualy will be set to false
-  // },
-  {
-    type: "Learn", // Can be "Release", "Learn", or "Podcast"
-    title: "What Is a Self-Custodial Wallet?",
-    description: "Your Guide to Financial Freedom with Blitz Wallet.",
-    htmlPageLink: "./self-custodial-wallet",
-    searchName: "self-custodial-wallet",
-    pageMetaDescription:
-      "Blitz Wallet is a fully open-source Bitcoin Lightning, Liquid, and eCash wallet that merges the fast capabilities of the Lightning Network with the security of self-custodial management at a price unmatched by competitors.",
-    readingLengthEST: 7, //In minutes
-    time: "1741611775672",
-    author: "Destiny Smart",
-    authorImage: "../../../public/authorImages/destiny_smart.webp", //Scoped to blog post page or public depending on image
-    imageUrl: "../../src/assets/images/info.svg", //Scoped to blog index.html| Megaphone is for Release, Info is for Learn and Microphone is for podcast
-    thumbnailUrl: "./self-custodial-wallet/assets/self_custody.png", //Scoped to blog index.html
-    isVertical: true,
-    isFeatured: false, // usualy will be set to false
-    customAuthor: true,
+    thumbnailUrl: "./introducing-blitz-wallet/assets/thumbnailUrlWhite.png", //Scoped to blog index.html
+    isFeatured: true, // usualy will be set to false
   },
 ];
 
