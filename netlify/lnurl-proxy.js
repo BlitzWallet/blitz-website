043a0004039bb581cf50476ec9d4b43d40c841f5
Blake Kaufman <blakekaufman9@gmail.com>
2026-05-01 17:01:34 -0400

    readding proxy

--- netlify/lnurl-proxy.js
+++ netlify/lnurl-proxy.js
@@ -1,16 +1,16 @@
-const UPSTREAM = 'createlnurlinvoice-6krimtymjq-uc.a.run.app';
+const UPSTREAM = "createlnurlinvoice-6krimtymjq-uc.a.run.app";
 
 exports.handler = async (event) => {
-  const host = event.headers.host || '';
-  const username = event.path.replace(/^\/(\.well-known\/lnurlp|p)\//, '');
-  const qs = event.rawQuery ? `?${event.rawQuery}` : '';
-  const url = `https://${UPSTREAM}/${username}${qs}`;
+  const host = event.headers.host || "";
+  const username = event.path.replace(/^\/(\.well-known\/lnurlp|p)\//, "");
+  const qs = event.rawQuery ? `?${event.rawQuery}` : "";
+  const url = `http://${UPSTREAM}/${username}${qs}`;
 
   const upstreamHeaders = {
-    'x-forwarded-host': host,
-    'x-forwarded-proto': 'https',
-    'user-agent': event.headers['user-agent'] || '',
-    'accept': event.headers['accept'] || '*/*',
+    "x-forwarded-host": host,
+    "x-forwarded-proto": "https",
+    "user-agent": event.headers["user-agent"] || "",
+    accept: event.headers["accept"] || "*/*",
   };
 
   const response = await fetch(url, {
@@ -20,11 +20,12 @@ exports.handler = async (event) => {
   });
 
   const body = await response.text();
-  const contentType = response.headers.get('content-type') || 'application/json';
+  const contentType =
+    response.headers.get("content-type") || "application/json";
 
   return {
     statusCode: response.status,
-    headers: { 'content-type': contentType },
+    headers: { "content-type": contentType },
     body,
   };
 };
